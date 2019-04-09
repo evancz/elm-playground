efc23ea9b49de7491172f48fba000bddf44875ea
Evan Czaplicki <evancz@users.noreply.github.com>
2019-04-09 13:50:29 -0400

    Update smiley.elm example for new API

--- examples/smiley.elm
+++ examples/smiley.elm
@@ -2,41 +2,35 @@ import Playground exposing (..)
 
 
 
-memory = 0
-
-
-draw window mouse keyboard memory =
-  [ circle lightYellow 200
-  , moveRight 80 (moveUp 50 (circle white 50))
-  , moveRight 75 (moveUp 40 (circle black 10))
-  , moveRight 80 (moveUp 100 (rectangle lightYellow 100 (50 + 50 * sin (degrees memory))))
-  , moveLeft 80 (moveUp 50 (circle white 50))
-  , moveLeft 75 (moveUp 40 (circle black 10))
-  , moveLeft 80 (moveUp 100 (rectangle lightYellow 100 (50 + 50 * sin (degrees memory))))
-  , moveDown 100 (oval black 180 40)
-  , moveDown 90 (oval lightYellow 180 40)
-  ]
-
-
-
--- WHEN THINGS CHANGE
-
-
-whenMouseIsClicked _ memory =
-  memory
-
-
-whenKeyIsPressed _ key memory =
-  memory
-
-
-whenTimePasses _ timePassed memory =
-  memory + 1
-
-
-
--- START EVERYTHING!
-
-
 main =
-  play memory draw whenMouseIsClicked whenKeyIsPressed whenTimePasses
+  picture
+    [ circle lightYellow 200
+
+    -- left eye
+    , circle white 50
+        |> moveLeft 70
+        |> moveUp 70
+    , circle black 10
+        |> moveLeft 75
+        |> moveUp 50
+    , rectangle lightYellow 100 50
+        |> moveLeft 70
+        |> moveUp 100
+
+    -- right eye
+    , circle white 50
+        |> moveRight 70
+        |> moveUp 70
+    , circle black 10
+        |> moveRight 65
+        |> moveUp 50
+    , rectangle lightYellow 100 50
+        |> moveRight 70
+        |> moveUp 100
+
+    -- mouth
+    , oval black 180 40
+        |> moveDown 100
+    , oval lightYellow 180 40
+        |> moveDown 90
+    ]
