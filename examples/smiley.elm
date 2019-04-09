58bad23269ef401ea2219105034071df62662744
Evan Czaplicki <evancz@users.noreply.github.com>
2019-04-09 13:55:20 -0400

    Lower the eyes to be more like human heads

--- examples/smiley.elm
+++ examples/smiley.elm
@@ -9,24 +9,24 @@ main =
     -- left eye
     , circle white 50
         |> moveLeft 70
-        |> moveUp 70
+        |> moveUp 50
     , circle black 10
         |> moveLeft 75
-        |> moveUp 50
+        |> moveUp 30
     , rectangle lightYellow 100 50
         |> moveLeft 70
-        |> moveUp 100
+        |> moveUp 80
 
     -- right eye
     , circle white 50
         |> moveRight 70
-        |> moveUp 70
+        |> moveUp 50
     , circle black 10
         |> moveRight 65
-        |> moveUp 50
+        |> moveUp 30
     , rectangle lightYellow 100 50
         |> moveRight 70
-        |> moveUp 100
+        |> moveUp 80
 
     -- mouth
     , oval black 180 40
